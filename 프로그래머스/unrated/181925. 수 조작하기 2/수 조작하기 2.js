27629e286f0fca3751cd932ef97b6c583a764e29
bt091006 <125196303+bt091006@users.noreply.github.com>
2023-04-20 21:33:37 +0900

    [unrated] Title: 수 조작하기 2, Time: 6.75 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,23 +1,4 @@
 function solution(numLog) {
-    let answer = '';
-    let start = numLog[0]   // 초기값
-    
-    for(let i = 1; i<numLog.length; i++){
-        let gap = numLog[i] - start;   // 이전값과 차이
-        
-        if(gap === 1){
-            answer += "w";
-        }
-        else if (gap === -1){
-            answer += "s";
-        }
-        else if (gap === 10){
-            answer += "d";
-        }
-        else if (gap === -10){
-            answer += "a";
-        }
-        start = numLog[i]
-    }
+    var answer = '';
     return answer;
 }
